ecb42883ab797f1e2f98d039fd12cb2f72f240a0
trantheanh3101 <trantheanhk11@gmail.com>
2024-07-03 23:32:07 +0700

    pc-data

---
+++ src/test/resources/students.sql
@@ -0,0 +1,18 @@
+create table students
+(
+    id       int auto_increment
+        primary key,
+    name     varchar(120) not null,
+    address  varchar(220) not null,
+    point    float        null,
+    id_class int          null,
+    constraint students_classroom_id_class_fk
+        foreign key (id_class) references classroom (id_class)
+);
+
+INSERT INTO student.students (id, name, address, point, id_class) VALUES (1, 'The Anh', 'Thanh hoa', 9, 1);
+INSERT INTO student.students (id, name, address, point, id_class) VALUES (2, 'Minh Tu', 'Ha Noi', 8.7, 2);
+INSERT INTO student.students (id, name, address, point, id_class) VALUES (3, 'Quoc Bao', 'Da Nang', 8.2, 2);
+INSERT INTO student.students (id, name, address, point, id_class) VALUES (4, 'Lan Huong', 'Hai Phong', 9.2, 1);
+INSERT INTO student.students (id, name, address, point, id_class) VALUES (14, 'trần thế anh', 'Mỹ Đình- Hà Nội', 9, 1);
+INSERT INTO student.students (id, name, address, point, id_class) VALUES (15, 'Alan Tran', 'Nam', 9, 1);
