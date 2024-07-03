3f8c973fac41b1885a9dab79b3ab96240e497c88
trantheanh3101 <trantheanhk11@gmail.com>
2024-07-03 21:59:46 +0700

    data

--- src/test/resources/student_students.sql
+++ src/test/resources/student_students.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `student` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `student`;
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
 -- Host: localhost    Database: student
@@ -16,6 +18,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `classroom`
+--
+
+DROP TABLE IF EXISTS `classroom`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `classroom` (
+  `id_class` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_class`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `classroom`
+--
+
+LOCK TABLES `classroom` WRITE;
+/*!40000 ALTER TABLE `classroom` DISABLE KEYS */;
+INSERT INTO `classroom` VALUES (1,'C03H1'),(2,'c04h1');
+/*!40000 ALTER TABLE `classroom` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `students`
 --
 
@@ -27,8 +53,11 @@ CREATE TABLE `students` (
   `name` varchar(120) NOT NULL,
   `address` varchar(220) NOT NULL,
   `point` float DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id_class` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `students_classroom_id_class_fk` (`id_class`),
+  CONSTRAINT `students_classroom_id_class_fk` FOREIGN KEY (`id_class`) REFERENCES `classroom` (`id_class`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +66,7 @@ CREATE TABLE `students` (
 
 LOCK TABLES `students` WRITE;
 /*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES (1,'The Anh','Thanh hoa',9.5),(2,'Minh Tu','Ha Noi',8.2),(3,'Quoc Bao','Da Nang',7.8),(4,'Lan Huong','Hai Phong',9),(5,'Khanh Linh','Hue',6.5);
+INSERT INTO `students` VALUES (1,'The Anh','Thanh hoa',9,1),(2,'Minh Tu','Ha Noi',8.7,2),(3,'Quoc Bao','Da Nang',8.2,2),(4,'Lan Huong','Hai Phong',9.2,1),(13,'trần thế anh','Mỹ Đình- Hà Nội',8.5,NULL),(14,'trần thế anh','Mỹ Đình- Hà Nội',9,1),(15,'Alan Tran','Nam',9,1);
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -50,4 +79,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-07-02 20:51:58
+-- Dump completed on 2024-07-03 21:59:20
